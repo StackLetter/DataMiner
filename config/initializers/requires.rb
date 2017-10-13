ab53274ba91e8cdffd5529802029d04ed3f93d4f
Matúš Salát <matus.salat@gmail.com>
2017-10-13 16:41:10 +0200

    Add puma config, fix production bug

--- config/initializers/requires.rb
+++ config/initializers/requires.rb
@@ -4,5 +4,5 @@ require 'sidekiq/cron/web'
 Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
 Sidekiq::Web.use Rack::Auth::Basic do |username, password|
   ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['sidekiq_login'])) &
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['sidekiq_password']))
 end if Rails.env.production?
