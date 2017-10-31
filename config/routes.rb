3e91adf01ebc8265c60a39c04a60bddd6f8aa787
Matúš Salát <matus.salat@gmail.com>
2017-10-31 22:41:27 +0100

    Add trivial recommender

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   root 'api#index'
   get 'evaluation' => 'evaluation#index'
 
+  scope 'trivial_recommendation', constraints: {domain: 'localhost'} do
+    get 'structure' => 'recommendation#index'
+    get 'top_new_questions' => 'recommendation#top_new_questions'
+    get 'greatest_hits' => 'recommendation#greatest_hits'
+    get 'answer_these' => 'recommendation#answer_these'
+  end
+
 end
