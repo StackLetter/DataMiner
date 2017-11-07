f6ca99fdb5fbdc2e0aef9595e8b2a2fc7cb9bd49
Matúš Salát <matus.salat@gmail.com>
2017-11-07 08:57:06 +0100

    Bug fix: fix missing scope for double level models

--- app/models/concerns/double_level_stack_api_model_concern.rb
+++ app/models/concerns/double_level_stack_api_model_concern.rb
@@ -3,6 +3,10 @@ module DoubleLevelStackApiModelConcern
 
   included do
 
+    def self.for_site(*_attrs)
+      self
+    end
+
     def self.find_model_object(api_item_response, site_id = 1)
       self.find_by(external_id: api_item_response['external_id'], site_id: site_id)
     end
