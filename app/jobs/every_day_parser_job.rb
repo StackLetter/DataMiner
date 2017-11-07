eb7a9aaf05d73a625cb85be2c24e6009cf68a0cf
Matúš Salát <matus.salat@gmail.com>
2017-11-07 18:24:22 +0100

    Optimize UserBadge mining task

--- app/jobs/every_day_parser_job.rb
+++ app/jobs/every_day_parser_job.rb
@@ -7,11 +7,11 @@ class EveryDayParserJob < ApplicationJob
       GenericParserJob.perform_later('Tag', 'new', site.id, {sort: 'name'})
 
       users_ids = User.for_site(site.id).order('RANDOM()').limit(500000).map(&:external_id)
-      GenericParserJob.perform_later('UserBadge', users_ids, site.id)
+      users_ids.each_slice(100) {|ids| GenericParserJob.perform_later('UserBadge', ids, site.id)}
 
       users = User.for_site(site.id).includes(:user_tags).where('account_id IS NOT NULL')
-      users_without_tags = users.select{ |user| user.user_tags.size == 0 }.map(&:external_id)
-      user_with_tags = users.select{ |user| user.user_tags.size > 0 }.map(&:external_id)
+      users_without_tags = users.select {|user| user.user_tags.size == 0}.map(&:external_id)
+      user_with_tags = users.select {|user| user.user_tags.size > 0}.map(&:external_id)
       UserTagParserJob.perform_later('all', site.id, users_without_tags) unless users_without_tags.empty?
       UserTagParserJob.perform_later('new', site.id, user_with_tags) unless user_with_tags.empty?
     end
