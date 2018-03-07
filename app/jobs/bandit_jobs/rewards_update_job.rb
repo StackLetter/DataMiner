3f27ccd9426554ba289ddcc2847c61f5fd5448b9
Matúš Salát <matus.salat@gmail.com>
2018-03-07 23:30:41 +0100

    Refactor reward method

--- app/jobs/bandit_jobs/rewards_update_job.rb
+++ app/jobs/bandit_jobs/rewards_update_job.rb
@@ -4,8 +4,10 @@
   def perform(segment_id, section_id, feedback, frequency = 'd')
     segment_section = MsaSegmentSection.includes(:msa_segment, :msa_section).find_by(segment_id: segment_id, section_id: section_id)
     all_segment_sections = segment_section.msa_segment.msa_segment_sections
+
+    history_max = MsaSegmentSectionRewardHistory.where(newsletter_type: frequency).where(segment_id: segment_id).first.sections_rewards.max
     specific_count = frequency == 'd' ? segment_section.daily_newsletters_count : segment_section.weekly_newsletters_count
-    value = all_segment_sections.map(&:reward).max * ((feedback.to_f / 100.0) * (1 / Math.log2(specific_count.to_f + 1)))
+    value = history_max * ((feedback.to_f / 100.0) * (1 / Math.log2(specific_count.to_f + 1)))
 
     ActiveRecord::Base.transaction do
       segment_section.update! reward: segment_section.reward + value
--- app/models/msa_segment_section_reward_history.rb
+++ app/models/msa_segment_section_reward_history.rb
@@ -2,4 +2,6 @@ class MsaSegmentSectionRewardHistory < ApplicationRecord
 
   belongs_to :msa_segment, foreign_key: :segment_id
 
+  default_scope -> { order(created_at: :desc) }
+
 end
